b6e655bc4a402d8226c0ffdd0af67835ca689ffe
Brad Serbu <bradserbu@bradserbu.com>
2016-12-10 14:07:38 -0500

    Adding examples script to demonstrating using nodus-run utiltiy to aid development and testing.

---
+++ examples.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# The directory where this file is located (not cwd)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Change to /examples directory
+cd $DIR
+
+# *** Various ways to lookup the company 'Uber' ***
+
+# Matches 'Uber' and 'UberMedia'
+./lookup-company-name.sh name="Uber"
+
+# Matches 'Uber' and 'UberMedia'
+./lookup-company-name.sh name="uber"
+
+# No Matches
+./lookup-company-name.sh name="uber" ignoreCase="false"
+
+# Matches 'UberMedia'
+./lookup-company-name.sh name="UberMedia"
+
+# Matches 'UberMedia'
+./lookup-company-name.sh name="UberM"
--- lookup-company-name.sh
+++ lookup-company-name.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# Helper script to test the Lookup Company by name activity
+# Helper script to examples the Lookup Company by name activity
 ./run.sh ./activities/companies/lookupCompanyName.js "$@";
