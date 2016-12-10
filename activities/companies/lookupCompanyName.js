4913d46eb1b75e9b9ea3ecb0e569b455f9a32ffd
Brad Serbu <bradserbu@bradserbu.com>
2016-12-10 13:50:30 -0500

    Added regex support to matching on company name.

--- activities/companies/lookupCompanyName.js
+++ activities/companies/lookupCompanyName.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// ** Constants
+const DEFAULT_IGNORE_CASE = true;
+
 // ** Dependencies
 const _ = require('underscore');
 const util = require('util');
@@ -7,18 +10,29 @@ const Activity = require('dataprocess').Activity;
 const DB = require('./db.json');
 
 /**
- * Search the DataFox Companies Database for a specified company name
- * @param name - The name of the company to search for
+ * Search the DataFox Companies Database for a specified company name.
+ * @param name - The name of the company to search for.
+ * @param ignoreCase - (default:true) Option to ignore case when matching string name.
  */
-function lookupCompanyName(name) {
+function lookupCompanyName(name, ignoreCase) {
+
+    // Argument Defaults
+    ignoreCase = util.isNullOrUndefined(ignoreCase)
+        ? DEFAULT_IGNORE_CASE
+        : util.isString(ignoreCase)
+        ? ignoreCase !== 'false'
+        : ignoreCase;
+
+    // Build a Regular Expression to search for name
+    const regex = new RegExp(name, ignoreCase ? 'i' : '');
 
     // STUB: Return the first entry from the database
-    return _.first(DB);
+    return _.filter(DB, company => regex.test(company['name']));
 }
 
 /**
  * Create an Activity to lookup a company by name.
- * - An Activity provides statistics, debug information, global error handlers, automatically
+ * - An Activity provides statistics, debug information, global error handlers, automatically.
  */
 const lookupCompanyNameActivity = Activity('lookup-company-name', lookupCompanyName);
 
@@ -28,7 +42,7 @@ const lookupCompanyNameActivity = Activity('lookup-company-name', lookupCompanyN
  * @param name
  * @returns {*}
  */
-module.exports = name => {
+module.exports = (name, ignoreCase) => {
 
     // Call the lookup company name activity
     return lookupCompanyNameActivity.run(name);
