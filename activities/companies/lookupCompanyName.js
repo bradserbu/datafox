50852454aac83b347fbbc7a56841ba3f0d70bb4c
Brad Serbu <bradserbu@bradserbu.com>
2016-12-10 14:35:18 -0500

    Custom matches property support.

--- activities/companies/lookupCompanyName.js
+++ activities/companies/lookupCompanyName.js
@@ -2,6 +2,7 @@
 
 // ** Constants
 const DEFAULT_IGNORE_CASE = true;
+const DEFAULT_MATCH = ['name', 'corporate_names'];
 
 // ** Dependencies
 const _ = require('underscore');
@@ -15,7 +16,7 @@ const DB = require('./db.json');
  * @param ignoreCase - (default:true) Option to ignore case when matching string name.
  * @param match - Optionaly specify which fields to search in the database.
  */
-function lookupCompanyName(name, ignoreCase) {
+function lookupCompanyName(name, ignoreCase, match) {
 
     // Argument Defaults
     ignoreCase = util.isNullOrUndefined(ignoreCase)
@@ -23,6 +24,14 @@ function lookupCompanyName(name, ignoreCase) {
         : util.isString(ignoreCase)
         ? ignoreCase !== 'false'
         : ignoreCase;
+
+    match = util.isNullOrUndefined(match)
+        ? DEFAULT_MATCH
+        : util.isArray(match)
+        ? match
+        : util.isString(match)
+        ? match.split(',')
+        : DEFAULT_MATCH;
 
     // Build a Regular Expression to search for name
     const regex = new RegExp(name, ignoreCase ? 'i' : '');
@@ -33,8 +42,14 @@ function lookupCompanyName(name, ignoreCase) {
     // Function to split name into words
     const words = text => text.match(/\w*\w/g);
 
+    // Function to check if a company property matches the company name
+    const matchProperty = (company, property) =>
+        util.isArray(company[property])
+            ? _.find(company[property], name => _.find(words(name), matchName))
+            : _.find(words(company[property]), matchName);
+
     // Function to check if any of the match properties contain the company name
-    const matchCompany = company => _.any(words(company['name']), matchName);
+    const matchCompany = company => _.any(match, property => matchProperty(company, property));
 
     // Search Database for any matching companies
     const matches = _.filter(DB, matchCompany);
@@ -55,8 +70,8 @@ const lookupCompanyNameActivity = Activity('lookup-company-name', lookupCompanyN
  * @param name
  * @returns {*}
  */
-module.exports = (name, ignoreCase) => {
+module.exports = (name, ignoreCase, match) => {
 
     // Call the lookup company name activity
-    return lookupCompanyNameActivity.run(name, ignoreCase);
+    return lookupCompanyNameActivity.run(name, ignoreCase, match);
 };
